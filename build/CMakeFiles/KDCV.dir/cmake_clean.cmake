3bcec4293f98def12d075f77781609f5ebac9a95
Isaac0424 <eodud3858@gmail.com>
2022-11-16 23:46:30 +0900

    opencv & CMake init

---
+++ build/CMakeFiles/KDCV.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KDCV.dir/main.cpp.o"
+  "KDCV.pdb"
+  "KDCV"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KDCV.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
